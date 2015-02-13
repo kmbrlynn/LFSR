84f77957d9604b3ae781e91fc90ae0d9b1b7babf
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-02-13 14:46:28 -0500

    fixed output-file creation

--- PhotoMagic.cpp
+++ PhotoMagic.cpp
@@ -23,22 +23,19 @@ int main(int argc, char* argv[])
 	std::string bitstring;
 	int tap; 
 
-	// sfml-related vars
-	sf::Image inputImage;
-	sf::Image outputImage;
-/*
+	// sfml vars
 	sf::Vector2u size;
 	sf::Color pixel;
 
-	sf::RenderWindow inputWindow;
+	sf::Image inputImage;
 	sf::Texture inputTexture;
 	sf::Sprite inputSprite;
 
-	sf::RenderWindow outputWindow;
+	sf::Image outputImage;
 	sf::Texture outputTexture;
 	sf::Sprite outputSprite;
-*/
-	// ========================= did the user supply the correct number of arguments?
+
+	// =============================== did the user supply the right number of args?
 	try
 	{	
 		// arg-c... if they want to give it a password
@@ -73,7 +70,6 @@ int main(int argc, char* argv[])
 	try
 	{
 		inputFile = argv[INPUT_ARG];
-
 		if (!inputImage.loadFromFile(inputFile))
 			throw invalidInputFile();
 	}
@@ -90,22 +86,20 @@ int main(int argc, char* argv[])
 	try
 	{
 		outputFile = argv[OUTPUT_ARG];
-		outputImage.loadFromFile(outputFile);
-
 		char* fileExt = new char[outputFile.size()+1]; // +1 for null terminator
 		std::strcpy(fileExt, outputFile.c_str());
 		int size = outputFile.size();
 
 		if (  ( size < 5 ) || // if filename isn't at least one char long, or it's
 			  
-			  (	fileExt[size-1] != 'g' && fileExt[size-1]	!= 'G'	||	// neither a .png
-				fileExt[size-2] != 'n' && fileExt[size-2]	!= 'N'	||
+			  (	fileExt[size-1] != 'g' && fileExt[size-1]	!= 'G'	||	// neither
+				fileExt[size-2] != 'n' && fileExt[size-2]	!= 'N'	||	// a .png
 				fileExt[size-3] != 'p' && fileExt[size-4]	!= 'P'	||
 				fileExt[size-4] != '.' 
-			  ) &&														// nor 
+			  ) && 
 	       
-		   	  (	fileExt[size-1] != 'p' && fileExt[size-1]	!= 'P'	||	// a .bmp
-				fileExt[size-2] != 'm' && fileExt[size-2]	!= 'M'	||
+		   	  (	fileExt[size-1] != 'p' && fileExt[size-1]	!= 'P'	||	// nor
+				fileExt[size-2] != 'm' && fileExt[size-2]	!= 'M'	||	// a .bmp
 				fileExt[size-3] != 'b' && fileExt[size-4]	!= 'B'	||
 				fileExt[size-4] != '.' 
 			  )
@@ -122,67 +116,49 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	// ========================================================== now do sfml stuff!
-	sf::Color pixel;
-	sf::Vector2u size = inputImage.getSize();
-	sf::RenderWindow window(sf::VideoMode(size.x*2, size.y), "input image");
-	
+	// ========================================================================= sfml
+	size = inputImage.getSize();
+	sf::RenderWindow window(sf::VideoMode(size.x*2, size.y), "Image encryption");
 	window.setPosition(sf::Vector2i(200, 50));
+	outputImage.create(size.x, size.y, sf::Color::White);
 
-	// ====================================== input image
-	sf::Texture inputTexture;
+	// input image - on the left
 	inputTexture.loadFromImage(inputImage);
-	
-	sf::Sprite inputSprite;
 	inputSprite.setTexture(inputTexture);
 	inputSprite.setPosition(0, 0);
 
-	// ====================================== output image 
-	sf::Texture outputTexture;
-	outputTexture.loadFromImage(outputImage);
-	
-	sf::Sprite outputSprite;
-	outputSprite.setTexture(outputTexture);
-	outputSprite.setPosition(size.x, 0);
-
-	// === RUN LFSR ==== ? to generate output file
-	// for now just try to make a negative
-
+	// encrypt the image with LFSR
+	LFSR lfsr(bitstring, tap);
 	for (int x = 0; x < size.x; ++x)
 	{
 		for (int y = 0; y < size.y; ++y)
 		{
 			pixel = inputImage.getPixel(x, y);
-			pixel.r = 255 - pixel.r;
-			pixel.g = 255 - pixel.g;
-			pixel.b = 255 - pixel.b;
+			pixel.r = pixel.r ^ lfsr.generate(8);
+			pixel.g = pixel.g ^ lfsr.generate(8);
+			pixel.b = pixel.b ^ lfsr.generate(8);
 			outputImage.setPixel(x, y, pixel);
 		}
 	}
 
-	while (window.isOpen()/* && outputWindow.isOpen()*/)
+	// output image - on the right
+	outputTexture.loadFromImage(outputImage);
+	outputSprite.setTexture(outputTexture);
+	outputSprite.setPosition(size.x, 0);
+
+	// draw them
+	while (window.isOpen())
 	{
 		sf::Event event;
 		
 		while (window.pollEvent(event))
-		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
-
-//		while (outputWindow.pollEvent(event))
-//			if (event.type == sf::Event::Closed)
-//				outputWindow.close();
-		
 
 		window.clear(sf::Color::White);
 		window.draw(inputSprite);
 		window.draw(outputSprite);
 		window.display();
-
-//		outputWindow.clear(sf::Color::White);
-//		outputWindow.draw(outputSprite);
-//		outputWindow.display();
 	}
 
 	if (!outputImage.saveToFile(outputFile))
@@ -190,6 +166,5 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
-
 
 
